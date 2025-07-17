84154edf15d4bd44380c4707aeceb5ba4f3e1a7c
dienifergraziela <dienifergrazielaripl@gmail.com>
2025-07-17 17:36:21 -0300

    terceiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/iBRsistemas-precos",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // cria 404.html na raiz
+          rename: '404.html'
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
