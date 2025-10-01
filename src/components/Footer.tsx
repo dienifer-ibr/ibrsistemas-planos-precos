5dbac613c99a768175437e2f66231a83008fcd95
dienifergraziela <dienifergrazielaripl@gmail.com>
2025-10-01 17:17:00 -0300

    Atualização 01/10 6

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Facebook, Instagram, Linkedin, Youtube } from "lucide-react";
 import { SiTiktok } from "react-icons/si";
+import '..//App.css';
 
 export const Footer = () => {
     const footerSections = [
